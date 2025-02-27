044a6f169012698e933be9c222114190e28c753c
Nirav <niravpatelp129@gmail.com>
2025-02-26 22:54:10 -0500

    * chore(projects): removed ProjectsPage component * refactor(InvoicesExample.tsx): removed unused import

--- src/app/projects/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProjectHierarchy from '@/components/ProjectPage/ProjectHierarchy';
-
-export default function ProjectsPage() {
-  return (
-    <div className='container py-6 h-screen'>
-      <ProjectHierarchy />
-    </div>
-  );
-}
