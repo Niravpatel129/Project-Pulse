b7454d488cc810c5a369cd6ebbf6a926d7ced3e2
Nirav <niravpatelp129@gmail.com>
2025-04-06 10:11:27 -0400

    2

--- src/app/[workspace]/(standaone)/approvals/[id]/page.tsx
+++ src/app/[workspace]/(standaone)/approvals/[id]/page.tsx
@@ -283,7 +283,7 @@ export default function ApprovalRequestPage() {
                       <TimelineItem
                         key={item.id}
                         step={item.id}
-                        className='group-data-[orientation=vertical]/timeline:ms-10 group-data-[orientation=vertical]/timeline:not-last:pb-8'
+                        className='group-data-[orientation=vertical]/timeline:ms-10 group-data-[orientation=vertical]/timeline:not-last:pb-10'
                       >
                         <TimelineHeader>
                           <TimelineSeparator className='group-data-[orientation=vertical]/timeline:-left-7 group-data-[orientation=vertical]/timeline:h-[calc(100%-1.5rem-0.25rem)] group-data-[orientation=vertical]/timeline:translate-y-6.5' />
@@ -294,16 +294,17 @@ export default function ApprovalRequestPage() {
                             </span>
                           </TimelineTitle>
                           <TimelineIndicator className='bg-primary/10 group-data-completed/timeline-item:bg-primary group-data-completed/timeline-item:text-primary-foreground flex size-6 items-center justify-center border-none group-data-[orientation=vertical]/timeline:-left-7'>
-                            {}
                             <Avatar className='size-6'>
                               <AvatarImage src={item.image} alt={item.title} />
-                              <AvatarFallback>{item.title[0]}</AvatarFallback>
+                              <AvatarFallback>{item.title ? item.title[0] : '?'}</AvatarFallback>
                             </Avatar>
                           </TimelineIndicator>
                         </TimelineHeader>
-                        <TimelineContent className='text-foreground mt-2 rounded-lg border px-4 py-3 whitespace-pre-wrap'>
+                        <TimelineContent className='text-foreground mt-2 mb-4 rounded-lg border px-4 py-3 whitespace-pre-wrap'>
                           {item.description}
-                          <TimelineDate className='mt-1 mb-0'>{item.date}</TimelineDate>
+                          <TimelineDate className='mt-1 mb-0'>
+                            {new Date(item.date).toLocaleString()}
+                          </TimelineDate>
                         </TimelineContent>
                       </TimelineItem>
                     );
