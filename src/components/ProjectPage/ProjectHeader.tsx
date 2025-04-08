6544014db89badff52fba2fc450eb05a36e2a81f
Nirav <niravpatelp129@gmail.com>
2025-04-08 07:16:38 -0400

    2

--- src/components/ProjectPage/ProjectHeader.tsx
+++ src/components/ProjectPage/ProjectHeader.tsx
@@ -435,15 +435,6 @@ export default function ProjectHeader() {
           ) : (
             <div className='container mx-auto px-4 py-1 sm:py-1'>
               <div className='flex flex-col sm:flex-row sm:items-center gap-3 sm:gap-4'>
-                <div className='relative h-8 w-8 sm:h-16 sm:w-16 rounded-md overflow-hidden'>
-                  <Image
-                    src='https://picsum.photos/200'
-                    alt='Project Thumbnail'
-                    fill
-                    className='object-cover'
-                    priority
-                  />
-                </div>
                 <div>
                   <h1 className='text-xl sm:text-2xl font-medium capitalize'>{project?.name}</h1>
                   <p className='text-xs sm:text-sm text-muted-foreground'>
