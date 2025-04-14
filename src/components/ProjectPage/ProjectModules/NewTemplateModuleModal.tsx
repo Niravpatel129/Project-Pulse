f5dcda92ac74966d9afd486b6b6b876b1eafb1ad
Nirav <niravpatelp129@gmail.com>
2025-04-14 14:52:18 -0400

    2

--- src/components/ProjectPage/ProjectModules/NewTemplateModuleModal.tsx
+++ src/components/ProjectPage/ProjectModules/NewTemplateModuleModal.tsx
@@ -233,6 +233,13 @@ export default function NewTemplateModuleModal({ isOpen, onClose, template, temp
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent className='max-w-5xl p-0 gap-0 '>
         <DialogTitle className='sr-only'>Create Order Template</DialogTitle>
+        <button
+          onClick={onClose}
+          className='absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground'
+        >
+          <X className='h-4 w-4' />
+          <span className='sr-only'>Close</span>
+        </button>
         <div className='flex w-full overflow-hidden rounded-lg bg-white'>
           {/* Left sidebar */}
           <div className='w-72 border-r border-gray-100 bg-white'>
