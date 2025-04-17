318e5faacd0c8743e85ec4751731061f0b65881e
Nirav <niravpatelp129@gmail.com>
2025-04-17 17:49:47 -0400

    2

--- src/app/[workspace]/projects/page.tsx
+++ src/app/[workspace]/projects/page.tsx
@@ -259,6 +259,16 @@ export default function ProjectsPage() {
             </p>
           </div>
           <div className='flex space-x-3'>
+            {isKanban && (
+              <button
+                onClick={() => {
+                  return setKanban(kanban === 'stages' ? 'status' : 'stages');
+                }}
+                className='inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4'
+              >
+                {kanban === 'stages' ? 'Switch to Status View' : 'Switch to Stage View'}
+              </button>
+            )}
             <PipelineSettings />
 
             <ProjectCreateForm
