6d67572252fe6594686917c60f25b45319907031
Nirav <niravpatelp129@gmail.com>
2025-04-19 02:02:50 -0400

    2

--- src/app/[workspace]/projects/[id]/components/ProjectWrapper.tsx
+++ src/app/[workspace]/projects/[id]/components/ProjectWrapper.tsx
@@ -1,6 +1,6 @@
 export default function ProjectWrapper({ children }: { children: React.ReactNode }) {
   return (
-    <div className='min-h-[calc(100vh-64px)] w-full mt-0 md:mt-4 flex flex-col gap-4 px-0 md:px-[10%] sm:px-[5%] xs:px-0 mx-auto overflow-y-auto pt-2'>
+    <div className='min-h-[calc(100vh-64px)] w-full mt-0 md:mt-4 flex flex-col gap-4 px-0 lg:px-[10%] xs:px-0 mx-auto overflow-y-auto pt-2'>
       {children}
     </div>
   );
