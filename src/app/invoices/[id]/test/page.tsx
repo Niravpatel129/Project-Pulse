698f84ce88325ac1e64c6bb4292c325f29cf4e3e
Nirav <niravpatelp129@gmail.com>
2025-05-07 19:16:07 -0400

    1

--- src/app/invoices/[id]/test/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function TestPage({ params }: { params: { id: string } }) {
-  return (
-    <div className='p-8'>
-      <h1 className='text-2xl font-bold'>Test Page</h1>
-      <p>Invoice ID: {params.id}</p>
-    </div>
-  );
-}
