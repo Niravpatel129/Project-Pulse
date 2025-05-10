fac5d2d24564f9eee654fc32f43dfd9416780404
Nirav <niravpatelp129@gmail.com>
2025-05-09 23:15:57 -0400

    2

--- src/app/[workspace]/main/page.tsx
+++ src/app/[workspace]/main/page.tsx
@@ -5,7 +5,6 @@ import Sidebar from '@/components/Main/Sidebar/Sidebar';
 import { ThemeProvider } from '@/components/ThemeProvider';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { GeistSans } from 'geist/font/sans';
-
 export default function Main() {
   return (
     <ThemeProvider defaultTheme='dark' enableSystem={false}>
@@ -14,8 +13,13 @@ export default function Main() {
           className={`relative flex min-h-screen w-full bg-background text-foreground ${GeistSans.className}`}
         >
           <Sidebar />
-          <main className='flex-1 w-full overflow-auto bg-background'>
-            <Invoices />
+          <main className='flex-1 w-full overflow-auto bg-background h-screen'>
+            <div className='flex h-full'>
+              <div className='w-full h-full'>
+                <Invoices />
+              </div>
+              <div className='w-full border-l border-l-gray-800 h-full'>Selected Invoice</div>
+            </div>
           </main>
         </div>
       </SidebarProvider>
