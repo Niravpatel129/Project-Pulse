eeca1e54f546a2506ff7b1dcc427045113f0aac7
Nirav <niravpatelp129@gmail.com>
2025-05-11 00:39:45 -0400

    1

--- src/app/[workspace]/layout.tsx
+++ src/app/[workspace]/layout.tsx
@@ -8,7 +8,7 @@ import { GeistSans } from 'geist/font/sans';
 import { usePathname } from 'next/navigation';
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
-  const isInvoiceRoute = pathname.includes('/invoice') || pathname.includes('/invoices');
+  const isInvoiceRoute = pathname.startsWith('/invoice/');
 
   return (
     <ThemeProvider defaultTheme='dark' enableSystem={false}>
