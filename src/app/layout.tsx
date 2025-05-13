1af610df0a8570e7b9cd1679b85f17aa8185a928
Nirav <niravpatelp129@gmail.com>
2025-05-13 17:25:45 -0400

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,6 @@ export async function generateMetadata(): Promise<Metadata> {
         },
       });
 
-      console.log('🚀 response:', response);
       if (response.ok) {
         const data = await response.json();
         if (data?.data?.logo) {
