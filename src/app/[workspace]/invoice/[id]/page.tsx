bdaa48c6fe66ed45942e21baca41e228a4c13b5e
Nirav <niravpatelp129@gmail.com>
2025-05-14 20:59:40 -0400

    cleanup

--- src/app/[workspace]/invoice/[id]/page.tsx
+++ src/app/[workspace]/invoice/[id]/page.tsx
@@ -86,7 +86,7 @@ export default function InvoicePage() {
         <PaymentSelector
           invoice={invoice}
           onSelectPayment={handleSelectPayment}
-          loading={paymentStatus === 'loading'}
+          loading={isLoading}
         />
       );
     }
