fe76f7ff678112deef0823bc4aae408ee57cd74e
Nirav <niravpatelp129@gmail.com>
2025-05-28 10:51:38 -0400

    2

--- src/app/[workspace]/dashboard/bills/layout.tsx
+++ src/app/[workspace]/dashboard/bills/layout.tsx
@@ -2,17 +2,17 @@ import { Metadata } from 'next';
 import React from 'react';
 
 export const metadata: Metadata = {
-  title: 'Bills | Dashboard',
-  description: 'Manage and track your bills and expenses in one place',
-  keywords: 'bills, expenses, financial management, dashboard',
+  title: 'Invoices | Dashboard',
+  description: 'Manage and track your Invoices and expenses in one place',
+  keywords: 'Invoices, expenses, financial management, dashboard',
   openGraph: {
-    title: 'Bills | Dashboard',
-    description: 'Manage and track your bills and expenses in one place',
+    title: 'Invoices | Dashboard',
+    description: 'Manage and track your Invoices and expenses in one place',
     type: 'website',
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Bills | Dashboard',
+    title: 'Invoices | Dashboard',
     description: 'Manage and track your bills and expenses in one place',
   },
 };
