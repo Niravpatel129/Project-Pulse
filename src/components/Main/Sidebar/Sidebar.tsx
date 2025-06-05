cbce1f49165d28bb6cc5f0c6bc45befe937bb04b
Nirav <niravpatelp129@gmail.com>
2025-06-05 12:56:54 -0400

    2

--- src/components/Main/Sidebar/Sidebar.tsx
+++ src/components/Main/Sidebar/Sidebar.tsx
@@ -139,7 +139,7 @@ export default function AppSidebar() {
           // },
           {
             name: 'Inbox',
-            href: '/dashboard/inbox',
+            href: '/dashboard/inbox/unassigned',
             icon: RiInboxFill,
           },
           // {
