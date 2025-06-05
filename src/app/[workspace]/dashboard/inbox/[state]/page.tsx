6bf1b892376358172c9ceab0a8319d0b6b8da1d7
Nirav <niravpatelp129@gmail.com>
2025-06-05 13:32:35 -0400

    2

--- src/app/[workspace]/dashboard/inbox/[state]/page.tsx
+++ src/app/[workspace]/dashboard/inbox/[state]/page.tsx
@@ -5,6 +5,7 @@ import InboxSidebar from '@/components/inbox/InboxSidebar';
 import { Button } from '@/components/ui/button';
 import { useSidebar } from '@/components/ui/sidebar';
 import { useInbox } from '@/hooks/use-inbox';
+import Link from 'next/link';
 import { useParams, useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { FiSettings, FiSidebar } from 'react-icons/fi';
@@ -62,21 +63,21 @@ const InboxPage = () => {
           <div className='w-full'>
             <div className='flex gap-6 overflow-x-auto'>
               {TABS.map((tab) => {
+                const path = tab.toLowerCase();
                 return (
-                  <button
+                  <Link
                     key={tab}
+                    href={`/dashboard/inbox/${path}`}
+                    prefetch={true}
+                    scroll={false}
                     className={`text-sm transition-colors duration-150 pb-1 border-b-2 whitespace-nowrap ${
                       activeTab === tab
                         ? 'font-semibold text-black dark:text-white border-black dark:border-white'
                         : 'font-normal text-gray-400 border-transparent hover:text-black dark:hover:text-white'
                     }`}
-                    onClick={() => {
-                      const path = tab.toLowerCase();
-                      router.push(`/dashboard/inbox/${path}`);
-                    }}
                   >
                     {tab}
-                  </button>
+                  </Link>
                 );
               })}
             </div>
