7d5077978b019c73cb175df417c1fdebbe6a0d80
Nirav <niravpatelp129@gmail.com>
2025-06-06 02:18:32 -0400

    2

---
+++ src/app/[workspace]/dashboard/inbox/[state]/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+import { useInbox } from '@/hooks/use-inbox';
+import { redirect } from 'next/navigation';
+
+const InboxPage = () => {
+  const { data: threads, error } = useInbox();
+
+  redirect(`/dashboard/inbox/unassigned/${threads?.pages[0]?.data[0]?.threadId || '0'}`);
+};
+
+export default InboxPage;
