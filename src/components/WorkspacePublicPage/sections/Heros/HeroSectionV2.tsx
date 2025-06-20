9a85d5673d35a4fb824ad3fc8ecc16b957bcde51
Nirav <niravpatelp129@gmail.com>
2025-06-20 03:27:01 -0400

    2

--- src/components/WorkspacePublicPage/sections/Heros/HeroSectionV2.tsx
+++ src/components/WorkspacePublicPage/sections/Heros/HeroSectionV2.tsx
@@ -20,20 +20,27 @@ export default function HeroSectionV2({ id }: { id: string }) {
           clipPath: 'polygon(0 0, 100% 0, 100% 88%, 50% 100%, 0 88%)',
         }}
       ></div>
-      <div className='flex flex-col items-center justify-center pt-20 relative z-10 text-white'>
+      <div className='flex flex-col items-center justify-center pt-32 relative z-10 text-white'>
         <Image
+          className='mb-16'
           src='https://firebasestorage.googleapis.com/v0/b/pulse-20181.firebasestorage.app/o/manual%2FGroup%201000004469.svg?alt=media&token=019151a4-31ba-41f1-9ecf-3d27a8c64bf6'
           alt='Hero Section'
           width={100}
           height={100}
         />
-        <div>Every project tells a story, what’s yours?</div>
-        <button>Create Now</button>
-        <div>
-          1 (844) 321 <span className='font-bold'>BOLO</span>
+        <div className='text-2xl font-semibold text-center my-8'>
+          Every project tells a story, what’s yours?
         </div>
-        <div>hello@bolocreate.com</div>
-        <div>3883 Nashua Drive, #9</div>
+        <button className='border-2 border-white text-white px-6 py-2 rounded-full bg-[#0C55BB] mb-8 hover:bg-[#0C55BB]/80 transition-all duration-300'>
+          Create Now
+        </button>
+        <div className='flex flex-col items-center justify-center text-white text-xl gap-4 font-medium mt-8'>
+          <div className='hover:underline cursor-pointer'>
+            1 (844) 321 <span className='font-extrabold'>BOLO</span>
+          </div>
+          <div className='hover:underline cursor-pointer'>hello@bolocreate.com</div>
+          <div className='hover:underline cursor-pointer'>3883 Nashua Drive, #9</div>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,10 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        sans: ['Inter', 'sans-serif'],
+      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
