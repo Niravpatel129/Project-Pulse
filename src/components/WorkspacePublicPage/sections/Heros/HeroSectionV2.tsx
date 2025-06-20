3dd64ac1ad8c70b145c69df7facd2912b6cedec5
Nirav <niravpatelp129@gmail.com>
2025-06-20 03:30:51 -0400

    2

--- src/components/WorkspacePublicPage/sections/Heros/HeroSectionV2.tsx
+++ src/components/WorkspacePublicPage/sections/Heros/HeroSectionV2.tsx
@@ -15,7 +15,7 @@ export default function HeroSectionV2({ id }: { id: string }) {
     >
       {/* Background Image */}
       <Image
-        src='https://firebasestorage.googleapis.com/v0/b/pulse-20181.firebasestorage.app/o/manual%2FIMG_7081%201.png?alt=media&token=01fcaa72-556d-4311-8469-91a3b49b07e3'
+        src='https://firebasestorage.googleapis.com/v0/b/pulse-20181.firebasestorage.app/o/manual%2FIMG_7081%201.png?alt=media'
         alt='Hero Background'
         fill
         priority
@@ -40,7 +40,7 @@ export default function HeroSectionV2({ id }: { id: string }) {
         {/* Fixed container to prevent layout shift */}
         <div className='w-[100px] h-[100px] mb-16 flex items-center justify-center'>
           <Image
-            src='https://firebasestorage.googleapis.com/v0/b/pulse-20181.firebasestorage.app/o/manual%2FGroup%201000004469.svg?alt=media&token=019151a4-31ba-41f1-9ecf-3d27a8c64bf6'
+            src='https://firebasestorage.googleapis.com/v0/b/pulse-20181.firebasestorage.app/o/manual%2FGroup%201000004469.svg?alt=media'
             alt='Hero Section'
             width={100}
             height={100}
